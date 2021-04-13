0a4bdbc3a29eeabb50523050536158df7bc7b1eb
danialeshete <danial.eshete@gmail.com>
2021-04-13 20:27:03 +0200

    works only with query and filters year

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -7,10 +7,12 @@ export default function SearchMovies() {
   //create the state for movies, and update that state appropriate
   const [movies, setMovies] = useState([]);
 
+  const [year, setYear] = useState();
+
   const searchMovies = async e => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false&year=${year}`;
 
     try {
       const res = await fetch(url);
@@ -34,6 +36,20 @@ export default function SearchMovies() {
           placeholder="i.e. Jurassic Park"
           value={query}
           onChange={e => setQuery(e.target.value)}
+        />
+        <span className="label" id="year">
+          1960
+        </span>
+        <input
+          type="range"
+          min="1900"
+          max="2021"
+          value={year}
+          onChange={e => {
+            setYear(e.target.value);
+            document.getElementById("year").innerHTML = e.target.value;
+          }}
+          className="range"
         />
         <button disabled={query.length < 1} className="button" type="submit">
           Search
